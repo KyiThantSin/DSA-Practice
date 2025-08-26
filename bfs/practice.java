e32ae454bcf025d57f6ae79e247867ecdb77bd92
KyiThantSin <kyits485@gmail.com>
2025-08-26 19:47:39 +0700

    is Exit Exists

--- bfs/practice.java
+++ bfs/practice.java
@@ -32,6 +32,36 @@ public class practice {
 
     }
 
+    public boolean isExitExist(int[][] images, int sr, int sc){
+        int rows = images.length;
+        int cols = images[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[rows][cols];
+
+        visited[sr][sc] = true;
+        queue.offer(new int[]{sr,sc});
+
+        int[][] dirs = {{0,1},{0,-1},{-1,0},{1,0}};
+        while(!queue.isEmpty()){
+            int[] cell = queue.poll();
+            
+            int r = cell[0];
+            int c = cell[1];
+            if(images[r][c] == 2) return true;
+            for(int[] dir: dirs){
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && !visited[nr][nc] && (images[nr][nc] == 0 || images[nr][nc] == 2)){
+                    queue.offer(new int[]{nr,nc});
+                    visited[nr][nc] = true;
+                }
+            }
+        }
+
+        return false;
+    }
     public void printGrid(int[][] grid){
         for(int[] rows: grid){
             for(int col: rows){
@@ -54,5 +84,14 @@ public class practice {
         System.out.println("After");
         p.floodFill(grids, 1, 1, 2);
         p.printGrid(grids);
+
+        int[][] images = {
+            {1,1,1},
+            {1,0,2},
+            {1,1,1},
+        };
+
+        System.out.println("Finding Exits");
+        System.out.println(p.isExitExist(images, 1, 1));
     }
 }
